e03d2c405ab744575d17dcda4480aae960f4f665
Efe TOKER <e.toker@medyatakip.com>
2022-05-17 13:59:49 +0300

    up

--- src.php
+++ src.php
@@ -3,8 +3,8 @@
 require 'vendor/autoload.php';
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-define('CONSUMER_KEY', 'E3SmtQWPJ1xw6r9xfHbEAf0ru');
-define('CONSUMER_SECRET', 'RnFU3Cej6l6XlidInNA9oGB0xffrc6ia8a87Vd87YZvUWs2mQY');
+define('CONSUMER_KEY', 'ZeBMFN5SOmgY7Ehou6JT6SfPR');
+define('CONSUMER_SECRET', 'kl7eGW7RcCM5MurrQRmlI0wErQLlUIEbQmjMVWcpc3LPLJ4yV6');
 define('OAUTH_CALLBACK', 'http://twitter-authenticator.com/callback.php');
 
 $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET);
